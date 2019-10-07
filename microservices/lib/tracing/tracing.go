864b5eaae67b620351adabee171931ec6e16e524
Carlos de Paula <me@carlosedp.com>
2019-10-07 18:58:06 -0300

    Load config from env variables

--- microservices/lib/tracing/tracing.go
+++ microservices/lib/tracing/tracing.go
@@ -7,28 +7,37 @@ import (
 	"net/http"
 	"reflect"
 	"runtime"
+	"time"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 )
 
 // Init returns an instance of Jaeger Tracer that samples 100% of traces and logs all spans to stdout.
 func Init(service string) (opentracing.Tracer, io.Closer) {
-	cfg := &config.Configuration{
+	// Add Opentracing instrumentation
+	defcfg := config.Configuration{
+		ServiceName: service,
 		Sampler: &config.SamplerConfig{
 			Type:  "const",
 			Param: 1,
 		},
 		Reporter: &config.ReporterConfig{
-			LogSpans: true,
+			LogSpans:            true,
+			BufferFlushInterval: 1 * time.Second,
 		},
 	}
-	tracer, closer, err := cfg.New(service, config.Logger(jaeger.StdLogger))
+
+	cfg, err := defcfg.FromEnv()
 	if err != nil {
-		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+		panic("Could not parse Jaeger env vars: " + err.Error())
 	}
+	tracer, closer, err := cfg.NewTracer()
+	if err != nil {
+		panic("Could not initialize jaeger tracer: " + err.Error())
+	}
+
 	return tracer, closer
 }
 

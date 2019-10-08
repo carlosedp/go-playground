1e2c4d71b9d192d073d4004aa27d77349d327e57
Carlos de Paula <me@carlosedp.com>
2019-10-08 17:20:12 -0300

    Update echo-tracing sample

--- echo-tracing/go.mod
+++ echo-tracing/go.mod
@@ -1,24 +1,17 @@
-module echo-tracing-metrics
+module echo-tracing
 
 go 1.12
 
 require (
-	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
-	github.com/gogo/protobuf v1.2.1 // indirect
-	github.com/gorilla/mux v1.7.3 // indirect
-	github.com/labstack/echo/v4 v4.1.10
-	github.com/labstack/gommon v0.3.0
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/opentracing/basictracer-go v1.0.0 // indirect
-	github.com/opentracing/opentracing-go v1.1.0
-	github.com/prometheus/client_golang v1.1.0
-	github.com/uber-go/atomic v1.4.0 // indirect
-	github.com/uber/jaeger-client-go v2.16.0+incompatible
-	github.com/uber/jaeger-lib v2.0.0+incompatible
-	go.elastic.co/apm v1.5.0
-	go.elastic.co/apm/module/apmot v1.5.0
-	go.uber.org/atomic v1.4.0 // indirect
-	sourcegraph.com/sourcegraph/appdash v0.0.0-20190731080439-ebfcffb1b5c0
-	sourcegraph.com/sourcegraph/appdash-data v0.0.0-20151005221446-73f23eafcf67 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/labstack/echo-contrib v0.6.0
+	github.com/labstack/echo/v4 v4.1.11
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
+	golang.org/x/net v0.0.0-20191007182048-72f939374954 // indirect
+	golang.org/x/sys v0.0.0-20191007154456-ef33b2fb2c41 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
+
+replace github.com/labstack/echo-contrib => github.com/carlosedp/echo-contrib v0.6.1-0.20191007230210-8b5ebe3ef46e
--- echo-tracing/server.go
+++ echo-tracing/server.go
@@ -1,97 +1,51 @@
 package main
 
 import (
-	mw "echo-tracing-metrics/middleware"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"runtime"
 	"strings"
 	"time"
 
+	"github.com/labstack/echo-contrib/jaegertracing"
+	// "github.com/labstack/echo-contrib/prometheus"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-
-	"github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/config"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	hitMetric = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "webserver_get_root",
-		Help: "The total number GET calls on each path",
-	}, []string{"code", "path"})
+func main() {
+	// Create new server
+	e := echo.New()
 
-	name string
-)
+	// Logging Middleware
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: metricsSkipper,
+	}))
 
-func init() {
-	prometheus.Register(hitMetric)
+	// Prometheus Metrics Middleware
+	// Depends on github.com/carlosedp/echo-contrib@prometheus
+	// p := prom.NewPrometheus("echo", nil)
+	// p.Use(e)
+
+	c := jaegertracing.New(e, metricsSkipper)
+	defer c.Close()
+
+	// Application routes
+	e.GET("/", rootHandler)
+	e.GET("/test/:name", testHandler)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
-// PromHitMetric is the middleware function to increase hit metric.
-func PromHitMetric(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if err := next(c); err != nil {
-			c.Error(err)
-		}
-		if c.Path() != "/metrics" {
-			hitMetric.WithLabelValues(fmt.Sprintf("%d", http.StatusOK), c.Path()).Inc()
-		}
-		return nil
-	}
-}
-
-// MetricsSkipper ignores metrics route on middleware
-func MetricsSkipper(c echo.Context) bool {
+// metricsSkipper ignores metrics route on some middleware
+func metricsSkipper(c echo.Context) bool {
 	if strings.HasPrefix(c.Path(), "/metrics") {
 		return true
 	}
 	return false
-}
-
-func main() {
-	// Add Opentracing instrumentation
-	cfg := config.Configuration{
-		Sampler: &config.SamplerConfig{
-			Type:  "const",
-			Param: 1,
-		},
-		Reporter: &config.ReporterConfig{
-			LogSpans:            true,
-			BufferFlushInterval: 1 * time.Second,
-		},
-	}
-	tracer, closer, _ := cfg.New(
-		"echo-tracing",
-		config.Logger(jaeger.StdLogger),
-	)
-	opentracing.SetGlobalTracer(tracer)
-	defer closer.Close()
-
-	// Create new server
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Skipper: MetricsSkipper}))
-	e.Use(PromHitMetric)
-	e.Use(mw.TraceWithConfig(mw.TraceConfig{
-		Tracer:  tracer,
-		Skipper: MetricsSkipper}))
-
-	// Application routes
-	e.GET("/", rootHandler)
-	e.GET("/test", testHandler)
-
-	// Route to Prometheus Metrics
-	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // Handlers
@@ -99,15 +53,11 @@ func rootHandler(c echo.Context) error {
 	os := runtime.GOOS
 	arch := runtime.GOARCH
 	time.Sleep(50 * time.Millisecond)
-	name = c.QueryParam("name")
+	name := c.QueryParam("name")
+
 	if name == "" {
-		fmt.Printf("%v", c)
-		if span := opentracing.SpanFromContext(c.Request().Context()); span != nil {
-			fmt.Printf("---> %v\n", span)
-			span.SetBaggageItem("Got OS/ARCH", fmt.Sprintf("%s/%s", os, arch))
-		} else {
-			fmt.Println("---> none\n")
-		}
+		// Wrap slowFunc on a new span to trace it's execution
+		jaegertracing.TraceFunction(c, slowFunc, "Test String")
 		time.Sleep(100 * time.Millisecond)
 		name = "World"
 	}
@@ -116,5 +66,32 @@ func rootHandler(c echo.Context) error {
 }
 
 func testHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Test path")
+	// Demonstrate Span creation for custom log/tag/baggage append
+	sp := jaegertracing.CreateChildSpan(c, "test handler")
+	defer sp.Finish()
+	var name = ""
+	name = c.Param("name")
+	sp.LogEvent("Called testHandler function, HTTP name param is: " + name)
+	sp.SetBaggageItem("name", name)
+	sp.SetTag("name_tag", "name")
+	time.Sleep(10 * time.Millisecond)
+
+	// Call slow function 5 times, it will create it's own span
+	ch := make(chan string)
+	for index := 0; index < 5; index++ {
+		// Do in parallel
+		go jaegertracing.TraceFunction(c, slowFunc, "Test String", ch)
+	}
+	for index := 0; index < 5; index++ {
+		fmt.Println(<-ch)
+	}
+
+	ret := fmt.Sprintf("Test path, name: %s", name)
+	return c.String(http.StatusOK, ret)
+}
+
+// A function to be wrapped
+func slowFunc(s string, c chan string) {
+	time.Sleep(time.Duration(rand.Intn(50)) * time.Millisecond)
+	c <- "received " + s
 }
